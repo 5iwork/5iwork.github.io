14a87fd3386664d4b7294dd7a77c07ee6c17620e
liming <liming@longshare.com>
2020-01-25 22:47:18 +0800

    初始化

---
+++ static/js/toc.js
@@ -0,0 +1,39 @@
+var toctitle = document.getElementById('toctitle');
+var path = window.location.pathname;
+if (toctitle != null) {
+    var oldtoc = toctitle.nextElementSibling;
+    var newtoc = document.createElement('div');
+    newtoc.setAttribute('id', 'tocbot');
+    newtoc.setAttribute('class', 'js-toc desktop-toc');
+    oldtoc.setAttribute('class', 'mobile-toc');
+    oldtoc.parentNode.appendChild(newtoc);
+    tocbot.init({
+        contentSelector: '#content',
+        headingSelector: 'h1, h2, h3, h4, h5',
+        positionFixedSelector: 'body',
+        fixedSidebarOffset: 50,
+        smoothScroll: false
+    });
+    if (!path.endsWith("index.html") && !path.endsWith("/")) {
+        var link = document.createElement("a");
+        if (document.getElementById('index-link')) {
+          indexLinkElement = document.querySelector('#index-link > p > a');
+          linkHref = indexLinkElement.getAttribute("href");
+          link.setAttribute("href", linkHref);
+        } else {
+          link.setAttribute("href", "index.html");
+        }
+        link.innerHTML = "<span><i class=\"fa fa-chevron-left\" aria-hidden=\"true\"></i></span> 返回首页";
+        var block = document.createElement("div");
+        block.setAttribute('class', 'back-action');
+        block.appendChild(link);
+        var toc = document.getElementById('toc');
+        var next = document.getElementById('toctitle').nextElementSibling;
+        toc.insertBefore(block, next);
+    }
+}
+
+var toctitle = document.getElementById('footer-text');
+if (toctitle != null) {
+    toctitle.append(" ©5i.work")
+}
